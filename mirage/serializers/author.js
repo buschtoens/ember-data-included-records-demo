2e5ec733127f7aaf3ce23b1c5ab5310425f1f904
Jan Buschtöns <buschtoens@gmail.com>
2017-06-20 19:29:27 +0200

    add 'related' link to author serializer

--- mirage/serializers/author.js
+++ mirage/serializers/author.js
@@ -1,5 +1,19 @@
 import { JSONAPISerializer } from 'ember-cli-mirage';
 
 export default JSONAPISerializer.extend({
-  include: ['blogPosts']
+  include: ['blogPosts'],
+
+  _getResourceObjectForModel(...args) {
+    const hash = JSONAPISerializer.prototype._getResourceObjectForModel.apply(this, args);
+
+    if (hash.relationships) {
+      for (const relationship of Object.values(hash.relationships)) {
+        relationship.links = {
+          related: '/show-stopper'
+        };
+      }
+    }
+
+    return hash;
+  }
 });
